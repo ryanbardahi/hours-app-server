ec0badc333e811c3ff7542ddcb7db06bed8a3721
ryan.bardahi <ryan.bardahi@gmail.com>
2024-11-30 18:56:02 +0800

    Fix corsOption origin for prod

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 4000;
 
 const corsOptions = {
-  origin: ["http://localhost:3000","http://hours-app-jade.vercel.app"],
+  origin: ["http://localhost:3000","https://hours-app-jade.vercel.app"],
   credentials: true,
   optionsSuccessStatus: 200
 };
